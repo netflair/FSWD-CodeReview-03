3cd8b2daf40ac69d8b290083e72eb2654844a464
Patrick Klostermann <dev@netflair.at>
2018-04-27 23:38:54 +0200

    Added Profile Cards and fixed header

--- js/main.js
+++ js/main.js
@@ -1,23 +1,16 @@
-var h = document.getElementById("header");
-var readout = document.getElementById("readout");
-var stuck = false;
-var stickPoint = getDistance();
+//Profile Cards
+var fritz = document.getElementById('fritz');
+var fran = document.getElementById('fran');
+var bill = document.getElementById('bill');
 
-function getDistance() {
-  var topDist = h.offsetTop;
-  return topDist;
-}
+fritz.innerHTML =	'<img src="' + profile[2].img + '">' + 
+					"<h2>" + profile[2].name + "</h2>" +
+					"<a href='mailto:" + profile[2].email + "'>" + profile[2].email + "</a>";
 
-window.onscroll = function(e) {
-  var distance = getDistance() - window.pageYOffset;
-  var offset = window.pageYOffset;
-  
-  if ( (distance <= 0) && !stuck) {
-    h.style.position = 'fixed';
-    h.style.top = '0px';
-    stuck = true;
-  } else if (stuck && (offset <= stickPoint)){
-    h.style.position = 'static';
-    stuck = false;
-  }
-}
+fran.innerHTML =	'<img src="' + profile[0].img + '">' + 
+					"<h2>" + profile[0].name + "</h2>" +
+					"<a href='mailto:" + profile[0].email + "'>" + profile[0].email + "</a>";
+
+bill.innerHTML =	'<img src="' + profile[1].img + '">' + 
+					"<h2>" + profile[1].name + "</h2>" +
+					"<a href='mailto:" + profile[1].email + "'>" + profile[1].email + "</a>";
